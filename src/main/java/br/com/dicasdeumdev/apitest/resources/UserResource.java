e39a0330191fb6199820218e839b8133939cd053
fvieira-code <fvieira.java@gmail.com>
2023-02-20 11:43:21 -0300

    Classe UserResource findById.

--- src/main/java/br/com/dicasdeumdev/apitest/resources/UserResource.java
+++ src/main/java/br/com/dicasdeumdev/apitest/resources/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.dicasdeumdev.apitest.resources;
 
 import br.com.dicasdeumdev.apitest.domain.User;
+import br.com.dicasdeumdev.apitest.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,8 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
+
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(new User(1, "Fernando", "fvieira@gmail.com", "luiz"));
+        return ResponseEntity.ok().body(service.finById(id));
     }
 }
