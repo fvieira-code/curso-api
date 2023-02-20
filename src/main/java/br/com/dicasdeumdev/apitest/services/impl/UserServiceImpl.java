6a7818de2caf90cc403eab70b8cb02a22432043b
fvieira-code <fvieira.java@gmail.com>
2023-02-20 11:42:43 -0300

    Classe UserServiceImpl.

---
+++ src/main/java/br/com/dicasdeumdev/apitest/services/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package br.com.dicasdeumdev.apitest.services.impl;
+
+import br.com.dicasdeumdev.apitest.domain.User;
+import br.com.dicasdeumdev.apitest.repositories.UserRepository;
+import br.com.dicasdeumdev.apitest.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public User finById(Integer id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
