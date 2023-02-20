7a9a9a4ccb1e673a302eaf6daff0c432ebb9400f
fvieira-code <fvieira.java@gmail.com>
2023-02-20 15:01:49 -0300

    Endpoint findAll para User.

--- src/main/java/br/com/dicasdeumdev/apitest/services/UserService.java
+++ src/main/java/br/com/dicasdeumdev/apitest/services/UserService.java
@@ -2,6 +2,10 @@ package br.com.dicasdeumdev.apitest.services;
 
 import br.com.dicasdeumdev.apitest.domain.User;
 
+import java.util.List;
+
 public interface UserService {
     User finById(Integer id);
+
+    List<User> findAll();
 }
