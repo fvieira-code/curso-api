006ae21b285d610425bb9f2a6234cdfe61c9ae3b
fvieira-code <fvieira.java@gmail.com>
2023-02-20 15:23:41 -0300

    Endpoint create para User.

--- src/main/java/br/com/dicasdeumdev/apitest/services/UserService.java
+++ src/main/java/br/com/dicasdeumdev/apitest/services/UserService.java
@@ -1,6 +1,7 @@
 package br.com.dicasdeumdev.apitest.services;
 
 import br.com.dicasdeumdev.apitest.domain.User;
+import br.com.dicasdeumdev.apitest.domain.dto.UserDTO;
 
 import java.util.List;
 
@@ -8,4 +9,6 @@ public interface UserService {
     User finById(Integer id);
 
     List<User> findAll();
+
+    User create(UserDTO obj);
 }
--- src/main/java/br/com/dicasdeumdev/apitest/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/dicasdeumdev/apitest/services/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package br.com.dicasdeumdev.apitest.services.impl;
 
 import br.com.dicasdeumdev.apitest.domain.User;
+import br.com.dicasdeumdev.apitest.domain.dto.UserDTO;
 import br.com.dicasdeumdev.apitest.repositories.UserRepository;
 import br.com.dicasdeumdev.apitest.services.UserService;
 import br.com.dicasdeumdev.apitest.services.exceptions.ObjectNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,9 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
     public User finById(Integer id) {
         Optional<User> obj = repository.findById(id);
@@ -24,5 +29,10 @@ public class UserServiceImpl implements UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    @Override
+    public User create(UserDTO obj) {
+        return repository.save(mapper.map(obj, User.class));
     }
 }
